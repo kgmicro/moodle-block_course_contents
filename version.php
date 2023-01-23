f9b10d888fb25b5183f089e1ad4a82b82d9ac686
Paul Davis <paul@pauld.jp>
2023-01-23 12:05:12 +0900

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_course_contents';
-$plugin->release = '6.0.0';
-$plugin->version = 2021021300;
+$plugin->release = '6.0.1';
+$plugin->version = 2021021400;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
